2e64b13685e0aa4c5cfa17e1ae4a0b45e1dff776
Yuralitv1982 <yuralitv02@gmail.com>
2025-01-13 16:17:24 +0200

    ch 3 video 12 split components

--- 03-react-essentials/src/App.jsx
+++ 03-react-essentials/src/App.jsx
@@ -1,48 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
-
+import Header from '../components/Header/Header.jsx';
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-   return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-   const description =
-      reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
-
-   return (
-      <header>
-         <img src={reactImg} alt='Stylized atom' />
-         <h1>React Essentials</h1>
-         <p>
-            {description} React concepts you will need for almost any app you
-            are going to build!
-         </p>
-      </header>
-   );
-}
-
-// function CoreConcept(props) {
-//    return (
-//       <li>
-//          <img src={props.image} alt='' />
-//          <h3>{props.title}</h3>
-//          <p>{props.description}</p>
-//       </li>
-//    );
-// }
-
-function CoreConcept({ image, title, description }) {
-   return (
-      <li>
-         <img src={image} alt='' />
-         <h3>{title}</h3>
-         <p>{description}</p>
-      </li>
-   );
-}
+import CoreConcept from '../components/CoreConcept.jsx';
 
 function App() {
    return (
@@ -62,6 +20,7 @@ function App() {
                   <CoreConcept {...CORE_CONCEPTS[3]} />
                </ul>
             </section>
+
             <h2>Time to get started!</h2>
          </main>
       </div>
