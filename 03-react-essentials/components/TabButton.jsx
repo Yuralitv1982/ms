25f71afd3974020b24870bb745b03f9b36a8f065
Yuralitv1982 <yuralitv02@gmail.com>
2025-01-13 16:45:12 +0200

    ch 3 video 14 add onClick to button

--- 03-react-essentials/components/TabButton.jsx
+++ 03-react-essentials/components/TabButton.jsx
@@ -1,8 +1,15 @@
 export default function TabButton(props) {
-   console.log(props);
+   // console.log(props);
+   // document.querySelector('button').addEventListener('click', () => {
+   //     do something
+   // })
+
+   function handleClick() {
+      console.log(`hello from button:${props.children}`);
+   }
    return (
       <li>
-         <button>{props.children}</button>
+         <button onClick={handleClick}>{props.children}</button>
       </li>
    );
 }
